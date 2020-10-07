d405759887a7770bb85ce97b4feb0b9941f5eeae
totyrola <astir.tadrous@polymtl.ca>
2020-10-07 22:49:06 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,7 @@ def order(values: list = None) -> bool:
     if values is None:
         values = [input("Veuillez entrer une valeure:") for _ in range(10)]   
     
-    return values == sorted(values)
+return values == sorted(values)
 
 
 
