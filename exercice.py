3910d8160c30af5a4dd4ad9a7ec249447a425c82
totyrola <astir.tadrous@polymtl.ca>
2020-10-21 15:05:51 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,6 +13,7 @@ def order(values: list = None) -> bool:
 def anagrams(words: list = None) -> bool:
     if words is None:
        words = [input("veuillez entrer deux mots:") for _ in range(2)]
+    
        
 
 
