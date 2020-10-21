92c5fb4da0340686e6988c39a913678233540766
totyrola <astir.tadrous@polymtl.ca>
2020-10-21 15:29:54 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -7,7 +7,7 @@ def order(values: list = None) -> bool:
     if values is None:
         values = [input("Veuillez entrer une valeure:") for _ in range(10)]   
     
-    return values == sorted(values))
+    returnes == sorted(values))
 
 def anagrams(words: list = None) -> bool:
     if words is None:
