ac9e0c005c7142197353a06c79c916fcdba7e092
totyrola <astir.tadrous@polymtl.ca>
2020-10-21 15:08:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,8 +13,9 @@ def order(values: list = None) -> bool:
 def anagrams(words: list = None) -> bool:
     if words is None:
        words = [input("veuillez entrer deux mots:") for _ in range(2)]
-    word_dicts = [{}, {}]   
 
+    word_dicts = [{}, {}]   
+    
 
 
 def contains_doubles(items: list) -> bool:
